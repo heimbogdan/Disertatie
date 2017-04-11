4013372cba6b9b9618cf72bcf5acecfbb872343d
Bogdan Heim <heim.bogdan@hotmail.com>
2017-04-11 13:54:25 +0300

    New Prj

---
+++ DynamicIntegrationAdministration/src/main/java/ro/helator/dia/scope/ScreenScoped.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package ro.helator.dia.scope;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
+
+/**
+ * 
+ * @author bogdan.heim
+ *
+ */
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Qualifier
+@Scope("screen")
+public @interface ScreenScoped {
+}
